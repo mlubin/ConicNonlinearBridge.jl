e14de5dc656431e720e5b990a2fb5d045fd9977c
Chris Coey <coey.chris@gmail.com>
2018-10-20 23:33:52 -0400

    remove old Convex.jl tests, update REQUIRE, add MPB conic model tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,31 +1,12 @@
 using ConicNonlinearBridge
-using Convex
+using MathProgBase
 using Ipopt
-using FactCheck
+using Base.Test
 
-tests = ["test_utilities.jl",
-         "test_affine.jl",
-         "test_lp.jl"]
-tests_socp = ["test_socp.jl"]
-tests_exp = ["test_exp.jl"]
+solver = ConicNLPWrapper(nlp_solver=IpoptSolver(print_level=0))
 
-println("Running tests:")
+include(Pkg.dir("MathProgBase", "test", "conicinterface.jl"))
 
-# The following syntax can be used to solve it using other solvers
-set_default_solver(ConicNLPWrapper(nlp_solver=IpoptSolver(print_level=0)))
-
-
-for curtest in tests
-    info(" Test: $(curtest)")
-    include(Pkg.dir("Convex","test",curtest))
-end
-
-for curtest in tests_socp
-    info(" Test: $(curtest)")
-    include(Pkg.dir("Convex","test",curtest))
-end
-
-for curtest in tests_exp
-    info(" Test: $(curtest)")
-    include(Pkg.dir("Convex","test",curtest))
+for testfun in [coniclineartest, conicSOCtest, conicSOCRotatedtest, conicEXPtest]
+    testfun(solver; duals=false, tol=1e-6)
 end
