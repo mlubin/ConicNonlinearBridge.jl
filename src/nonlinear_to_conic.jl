177892e3c9f6c4aeaff880e7746bdd4b83f7f78e
ccoffrin <carleton@coffrin.com>
2018-10-23 10:29:52 -0600

    fix package scope of getobjbound and getsolvetime

--- src/nonlinear_to_conic.jl
+++ src/nonlinear_to_conic.jl
@@ -247,9 +247,9 @@ end
 
 MathProgBase.status(m::NonlinearToConicBridge) = m.status
 MathProgBase.getobjval(m::NonlinearToConicBridge) = m.objval
-MathProgBase.getobjbound(m::NonlinearToConicBridge) = MathProgBase.getobjbound(m.nlp_model)
+MathProgBase.getobjbound(m::NonlinearToConicBridge) = getobjbound(m.nlp_model)
 MathProgBase.getsolution(m::NonlinearToConicBridge) = m.solution[1:size(m.A_ini,2)]
-MathProgBase.getsolvetime(m::NonlinearToConicBridge) = MathProgBase.getsolvetime(m.nlp_model)
+MathProgBase.getsolvetime(m::NonlinearToConicBridge) = getsolvetime(m.nlp_model)
 
 MathProgBase.numvar(m::NonlinearToConicBridge) = m.numVar
 # is numconstr well-defined for conic models?
